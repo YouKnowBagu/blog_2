470e8fb6235b1ca6b8697a3960de1c40ed686693
Sean <sean.p.sampson@gmail.com>
2017-06-09 20:48:21 -0600

    Add test cases

--- blog/polls/models.py
+++ blog/polls/models.py
@@ -5,14 +5,16 @@ from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        
 class Choice(models.Model):
     def __str__(self):
         return self.choice_text
